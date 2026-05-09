013843e43605f2dc676c1b234ef8f320150b34ee
valekmastersaa-cpu <valekmastersaa@gmail.com>
2026-05-09 21:44:31 +0500

    Первый каркас

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'mudrets-cache-v1';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'manifest.json'
+];
+
+// Установка: кэшируем основные файлы
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Активация: чистим старые кэши
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+// Стратегия: сначала кэш, потом сеть
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
